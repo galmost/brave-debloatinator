792536c33a4e776c261e62a659b179baf9c98df7
Dru <galmost@protonmail.com>
2026-01-03 01:30:59 +0200

    Remove policies.json after installation
    Remove policies.json after copying to the target directory.

--- brave-bullshitinator-linux-install.sh
+++ brave-bullshitinator-linux-install.sh
@@ -6,6 +6,5 @@ sudo mkdir /etc/brave/policies/managed -p || exit 0
 # Download file
 ls brave-bullshitinator-linux-install.sh || wget https://github.com/galmost/brave-debloatinator/blob/main/brave-bullshitinator-linux-install.sh || exit 0
 sudo cp policies.json /etc/brave/policies/managed || exit 0
-ls policies.json && rm policies.json
 # End
 echo "Brave debullshitinator installed"
